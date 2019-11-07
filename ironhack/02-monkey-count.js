00f52c2997aae507abc8c82f108309939c07dc34
Marti D <53455596+mrsdo@users.noreply.github.com>
2019-11-06 20:14:05 -0500

    Updated file mapping names to exercises. Updated README.md to reflect these changes.

---
+++ ironhack/02-monkey-count.js
@@ -0,0 +1,72 @@
+/*
+Ironhack Prep Work: Monkey Count
+Author: Martinique Dolce, me@martidolce.com
+Date: November 6, 2019
+**********************
+
+Challenge:
+You take your son to the forest to see the monkeys. You know that there are a certain number there (n),
+but your son is too young to just appreciate the full number, he has to start counting them from 1.
+
+As a good parent, you will sit and count with him. Given the number (n), populate an array with all numbers
+up to and including that number, but excluding zero.
+
+monkeyCount(10) // --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+monkeyCount(1) // --> [1]
+**********************
+
+JavaScript Operators used...
+
+Array.from(), Loops (For, While, Do While)
+
+function monkeyCount(n) {
+// your code here
+}monkeyCount(n);
+
+ */
+
+/*i = 0;
+function monkeyCount(n) {
+
+    while (i < n ){
+        i++;
+        //console.log(i);
+        //console.log([i]);
+        console.log(i);
+    }
+}monkeyCount(15);
+
+let n = 12345;
+
+const i = Array.from(String(n), Number);
+
+console.log(i);   //[1,2,3,4,5]
+
+*/
+/*let i = 0;
+
+
+
+function monkeyCount(n){
+
+    while (i < n ) {
+
+        i++ ;
+        console.log(i);
+    }
+
+}monkeyCount(10);*/
+
+let i = 0;
+let n = Array.from(String(i), Number);
+function monkeyCount(n) {
+
+    while (i < n ){
+        i++;
+        //console.log(i);
+
+        console.log(i);
+    }
+}monkeyCount(15);
+
+
